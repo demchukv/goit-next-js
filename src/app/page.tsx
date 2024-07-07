1f98b6876ae7878ebc95ca0ba3bd5ea80ea98b9e
Volodymyr Demchuk <demchukv@gmail.com>
2024-07-07 10:32:43 +0300

    CSS modules in labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from "./components/active-label";
+import NotActiveLabel from "./components/not-active-label";
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <ActiveLabel>Active label</ActiveLabel>
+      <NotActiveLabel>Not active label</NotActiveLabel>
     </main>
   );
 }
